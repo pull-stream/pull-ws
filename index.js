f83217feb63ab961ddaa50922e20ef14d9f3303e
Damon Oehlman <damon.oehlman@gmail.com>
2014-11-12 08:36:18 +1100

    Remove gendocs docs generation

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/**
-  # pull-ws
-
-  A simple (but effective) implementation of a
-  [`pull-stream`](https://github.com/dominictarr/pull-stream) `Source` and `Sink`
-  that is compatible both with native browser WebSockets and
-  [`ws`](https://github.com/einaros/ws) created clients.
-
-  ## Reference
-
-**/
 exports = module.exports = duplex;
 
 exports.source = require('./source');
