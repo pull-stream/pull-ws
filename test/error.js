226b0adad13c84c194e2315861f19eb20ac791a2
Dominic Tarr <dominic.tarr@gmail.com>
2014-11-12 00:53:30 +0100

    check that source is aborted too

--- test/error.js
+++ test/error.js
@@ -4,17 +4,30 @@ var endpoint = require('./helpers/wsurl') + '/read';
 var pull = require('pull-stream');
 var ws = require('../');
 
-test('test error', function (t) {
 
+//connect to a server that does not exist, and check that it errors.
+//should pass the error to both sides of the stream.
+test('test error', function (t) {
+  var _err
   pull(
     pull.values(['x', 'y', 'z']),
-    //attempt to connect to a server that does not exist.
+    pull.through(null, function (err) {
+      if(_err) {
+        t.strictEqual(err, _err);
+        t.end();
+      }
+      _err = err
+    }),
     ws(new WebSocket('ws://localhost:34897/' + Math.random())),
     pull.collect(function (err) {
-      t.ok(err)
-      t.end()
+      if(_err) {
+        t.strictEqual(err, _err);
+        t.end();
+      }
+      _err = err
     })
   )
 
 })
+
 
