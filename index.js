e3713817ebfc2d2488f2c1d2422d4e8c996f1259
Dominic Tarr <dominic.tarr@gmail.com>
2016-04-05 16:52:04 +1200

    expose http request properties

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ exports.source = require('./source');
 exports.sink = require('./sink');
 
 function duplex (ws, opts) {
+  var req = ws.upgradeReq || {}
   return {
     source: exports.source(ws),
-    sink: exports.sink(ws, opts)
+    sink: exports.sink(ws, opts),
+
+    //http properties - useful for routing or auth.
+    headers: req.headers,
+    url: req.url,
+    upgrade: req.upgrade,
+    method: req.method
   };
 };
+
