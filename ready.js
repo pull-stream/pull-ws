2f1da03016c8fb9aa8642b8244b463920bfbe34e
Dominic Tarr <dominic.tarr@gmail.com>
2019-06-06 14:16:40 +0200

    use strict

--- ready.js
+++ ready.js
@@ -1,3 +1,4 @@
+'use strict'
 module.exports = function(socket, callback) {
   var remove = socket && (socket.removeEventListener || socket.removeListener);
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict'
 var ws = require('./')
 var WebSocket = require('ws')
 var http = require('http')
